fa54c86dc8e4729bce51cf56309b49325102144c
unknown <cristianaaguirre0@gmail.com>
2022-02-03 09:48:04 -0300

    Iniciando con Javascript

---
+++ fifth-excercise/js/01.js
@@ -0,0 +1,30 @@
+// Variables
+
+var product = 'Audifonos Gamers'; //iniciar variable y darle valor
+
+var disponible; // se inicia la variable pero no le damos valor / NO OLVIDAR DE USAR EL PUNTO Y COMA, PARA TENER EL HABITO
+
+product = true; // reasignar el valor de la variable
+
+disponible = true; 
+
+var product = "Computadoras",
+    disponible = false,
+    categorias = "Computadoras"; // Inicializar multiples variables sin usar la palabra VAR
+
+    /*var 1disponible 
+    var -disponible / Estas sintaxis son erroneas, pero si puedes iniciar una variable con _*/
+
+/*----------------------------------------------------------------------------------------------------------------*/
+
+//Estilos para Variables cuando hay mas de una palabra
+
+var palabra_magica // se conoce como underscore
+var palabraMagica // se conoce como Camelcase
+var PalbraMagica // se conoce como Pascal Case
+
+console.log(product) 
+
+ /*Java Script es case sensitive, eso quiere decir que si tengo una variable llamada product y luego quiero usarla
+debo llamar la variable tal y cual esta escrita. No podria en todo caso poner Product o PRODUCT, pues no es su nombre
+original*/
