73c3859492b060ee59af9b3e25459d016b99860c
unknown <cristianaaguirre0@gmail.com>
2022-02-05 11:26:46 -0300

    Practica con validacion de formulario

--- sixth-excercise/js/app.js
+++ sixth-excercise/js/app.js
@@ -68,3 +68,62 @@ btnEnviar.addEventListener('click', function (evento) {
     evento.preventDefault();
     console.log('Validando formulario')
 })*/
+/* ------------------------------------------------------------------------------------------ */
+
+//Eventos de inputs y text-area - Evento Submit
+
+// Validar el formulario
+
+const datos = {
+    nombre : "",
+    email : "",
+    mensaje : "",
+}
+
+/* Variables del formulario */
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+nombre.addEventListener('input', escribir)
+email.addEventListener('input', escribir)
+mensaje.addEventListener('input', escribir)
+
+//Evento Submit
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    //Validar Formulario
+    const {nombre, email, mensaje} = datos;
+
+    if (nombre == "" & email == "" & mensaje == "") {
+        crearAlerta("*Todos los campos son obligatorios", "error");
+        return//Bloquea codigo hasta el final del if
+    }
+
+        crearAlerta('*El formulario se ha enviado correctamente');
+});
+
+/*Funciones*/
+
+function escribir (e) {
+    datos[e.target.id] = e.target.value;
+}
+
+function crearAlerta (mensaje, error = null) {
+    const alerta = document.createElement('P');
+    alerta.textContent = mensaje;
+
+    if (error) {
+        alerta.classList.add('error');
+    } else {
+        alerta.classList.add('enviado')
+    }
+
+    formulario.appendChild(alerta)
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
