a73e4f7512d9c6f51554ca3d67441eee5178edb9
unknown <cristianaaguirre0@gmail.com>
2022-02-10 08:06:44 -0300

    subiendo codigo js para el menu

--- seventh-excercise/src/js/app.js
+++ seventh-excercise/src/js/app.js
@@ -1,2 +1,13 @@
 const openMenu = document.querySelector('.menu');
-console.log(openMenu)
+const menuBars = document.querySelector('.header__nav');
+const closeMenu = document.querySelectorAll('a.nav__option');
+
+openMenu.addEventListener('click', () => {
+    menuBars.classList.toggle('open-menu');
+})
+
+for (let i = 0; i < closeMenu.length; i++) {
+    closeMenu[i].addEventListener('click', () => {
+        menuBars.classList.remove('')
+    })
+}
