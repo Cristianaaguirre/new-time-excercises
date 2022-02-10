2d25402877fe106dc53f956d3945890697bd194b
unknown <cristianaaguirre0@gmail.com>
2022-02-10 18:31:59 -0300

    finalizando la navegacion responsive

--- seventh-excercise/src/js/app.js
+++ seventh-excercise/src/js/app.js
@@ -3,11 +3,11 @@ const menuBars = document.querySelector('.header__nav');
 const closeMenu = document.querySelectorAll('a.nav__option');
 
 openMenu.addEventListener('click', () => {
-    menuBars.classList.toggle('open-menu');
+    menuBars.classList.toggle('header__nav--modifier');
 })
 
 for (let i = 0; i < closeMenu.length; i++) {
     closeMenu[i].addEventListener('click', () => {
-        menuBars.classList.remove('')
+        menuBars.classList.remove('header__nav--modifier')
     })
 }
