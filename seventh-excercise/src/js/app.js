b2c8b7ce0ec713c5052a66717d24c0556cf76900
unknown <cristianaaguirre0@gmail.com>
2022-02-10 21:39:03 -0300

    estilando la galeria de imagenes

--- seventh-excercise/src/js/app.js
+++ seventh-excercise/src/js/app.js
@@ -11,3 +11,21 @@ for (let i = 0; i < closeMenu.length; i++) {
         menuBars.classList.remove('header__nav--modifier')
     })
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    createGallery();
+})
+
+function createGallery () {
+    const galeria = document.querySelector('.crear-galeria');
+
+    for (let i = 1; i <= 12; i++) {
+        const img = document.createElement('IMG');
+        img.src = `/src/img/thumb/${i}.jpg`;
+        
+        const list = document.createElement('LI');
+        list.appendChild(img)
+        
+        galeria.appendChild(list);
+    }
+}
